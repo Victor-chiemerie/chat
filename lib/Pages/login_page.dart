e69c2bac2256031d7cf03c36f1502aacbb13428a
Victor-chiemerie <victorchiemerie02@gmail.com>
2024-02-21 23:58:04 +0100

    Initialize chat

---
+++ lib/Pages/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late GlobalKey<FormState> _formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold();
+  }
+}
